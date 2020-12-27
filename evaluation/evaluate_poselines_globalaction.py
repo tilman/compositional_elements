9f686295947cec3166578ea8af1f82e9883464b5
Tilman Marquart <til.marquart@gmail.com>
2020-12-27 14:59:32 +0100

    updated evaluation script

--- evaluation/evaluate_poselines_globalaction.py
+++ evaluation/evaluate_poselines_globalaction.py
@@ -8,20 +8,30 @@ from tqdm import tqdm
 from compositional_elements.generate import global_action, pose_abstraction, pose_direction
 from compositional_elements.visualize import visualize
 from compositional_elements.detect import converter
+from compositional_elements.detect.faster_rcnn import get_person_boundingboxes
+from compositional_elements.detect.hrnet import get_pose_keypoints
 # from compositional_elements.detect.lib.EnhancePoseEstimation.src.lib import person_detection, pose_estimation
-from compositional_elements.detect.lib.PoseBasedRetrievalDemo.src.API.lib import person_detection, pose_estimation
+# sys.path.append("./compositional_elements/detect/lib/PoseBasedRetrievalDemo/src/API")
+# from compositional_elements.detect.lib.PoseBasedRetrievalDemo.src.API.lib import person_detection, pose_estimation
 INPUT_DIR = "/Users/tilman/Documents/Programme/Python/new_bachelor_thesis/datasets/old_icc/icc_images_imdahl/"
-OUTPUT_DIR = "/Users/tilman/Documents/Programme/Python/new_bachelor_thesis/evaluation/compositional_elements/v0.0.1_22.12.20/icc_images_imdahl"
+OUTPUT_DIR = "/Users/tilman/Documents/Programme/Python/new_bachelor_thesis/evaluation/compositional_elements/v0.0.2_27.12.20/icc_images_imdahl"
 
 def get_icc(output_dir, img_path):
     basename = os.path.basename(img_path)
     img = cv2.imread(img_path)
-    _, _, det_data = person_detection.person_detection(img_path=img_path, person_detector="Faster R-CNN")
-    hrnet_output = pose_estimation.pose_estimation(detections=det_data["detections"],
-                                centers=det_data["centers"],
-                                scales=det_data["scales"],
-                                img_path=img_path,
-                                keypoint_detector="Baseline HRNet")
+    # _, _, det_data = person_detection.person_detection(img_path=img_path, person_detector="Faster R-CNN")
+    # hrnet_output = pose_estimation.pose_estimation(detections=det_data["detections"],
+    #                             centers=det_data["centers"],
+    #                             scales=det_data["scales"],
+    #                             img_path=img_path,
+    #                             keypoint_detector="Baseline HRNet")
+    person_boundingboxes = get_person_boundingboxes(img_path)
+    # debug test: is person_boundingboxes["detections"] already an image
+
+    # _, _, det_data = person_detection.person_detection(img_path=img_path, person_detector="Faster R-CNN")
+    hrnet_output = get_pose_keypoints(detections=person_boundingboxes["detections"],
+                                centers=person_boundingboxes["centers"],
+                                scales=person_boundingboxes["scales"])
     poses = converter.hrnet_to_icc_poses(hrnet_output)
     global_action_lines = global_action.get_global_action_lines(poses)
     pose_lines = pose_abstraction.get_pose_lines(poses)
