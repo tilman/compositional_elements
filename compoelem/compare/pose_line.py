daf664cb1d489c1ccb8737e82023d321a2be7a26
Tilman Marquart <til.marquart@gmail.com>
2021-01-15 21:32:13 +0100

    add plots

--- compoelem/compare/pose_line.py
+++ compoelem/compare/pose_line.py
@@ -1,4 +1,5 @@
 from compoelem.types import *
+from compoelem.config import config
 
 #optimize with KD trees or faiss: https://github.com/facebookresearch/faiss
 
@@ -15,6 +16,8 @@ def compare_pose_line(a: PoseLine, b: PoseLine) -> float:
     return np.mean([a.top.distance(b.top), a.bottom.distance(b.bottom)])
 
 def find_nearest_pose_line_distance(target: PoseLine, lines: Sequence[PoseLine]) -> Tuple[float, int]:
+    # if(len(lines)):
+    #     return config["compare"]["no_pose_line_penalty"]
     comparisons: Sequence[Tuple[float, int]] = []
     for idx, line in enumerate(lines):
         mean_distance = compare_pose_line(line, target)
@@ -24,11 +27,13 @@ def find_nearest_pose_line_distance(target: PoseLine, lines: Sequence[PoseLine])
     return nearest_pose_line
 
 def compare_pose_lines(a: Sequence[PoseLine], b: Sequence[PoseLine]) -> float:
+    if(len(b) == 0):
+        return len(a) * config["compare"]["no_pose_line_penalty"] 
     nearest_pose_line_distances: Sequence[Tuple[float, int]] = []
     for query_pose_line in a:
         nearest_pose_line_distances.append(find_nearest_pose_line_distance(query_pose_line, b))
     nearest_pose_line_distances = np.array(nearest_pose_line_distances)
-    nearest_pose_line_sum = np.sum(nearest_pose_line_distances[:,0])
+    nearest_pose_line_sum = np.sum(nearest_pose_line_distances[:, 0])
     penalty_pose_line_indices = [idx for idx in range(0,len(b)) if idx not in nearest_pose_line_distances[:,1]]
     penalty_pose_lines: Sequence[PoseLine] = np.array(b)[penalty_pose_line_indices]
     if len(penalty_pose_lines) > 0:
