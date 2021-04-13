cfd14a59635695c9a7b561214cfa8be2064d7d61
Tilman Marquart <til.marquart@gmail.com>
2021-04-13 22:21:50 +0200

    add eval2

---
+++ final_evaluation/eval_summary_final2.py
@@ -0,0 +1,31 @@
+import pickle
+import os
+import numpy as np
+import pandas as pd
+
+osuname = os.uname().nodename
+print("osuname", osuname)
+if osuname == 'MBP-von-Tilman' or osuname == 'MacBook-Pro-von-Tilman.local':
+    COMPOELEM_ROOT = "/Users/tilman/Documents/Programme/Python/new_bachelor_thesis/compoelem"
+elif osuname == 'lme117':
+    COMPOELEM_ROOT = "/home/zi14teho/compositional_elements"
+else:
+    COMPOELEM_ROOT = os.getenv('COMPOELEM_ROOT')
+DATASTORE_NAME = "combined_datastore_ceb_dataset"
+DATASTORE_FILE = COMPOELEM_ROOT+"/final_evaluation/"+DATASTORE_NAME+".pkl"
+EVAL_RESULTS_FILE_DIR = COMPOELEM_ROOT+"/final_evaluation/final2pkl/"
+
+
+evaluation_log = [pickle.load(open(EVAL_RESULTS_FILE_DIR+"/"+logfile, "rb")) for logfile in os.listdir( EVAL_RESULTS_FILE_DIR )]
+# new_log_entries = list(filter(lambda log_entry: log_entry["new"], evaluation_log))
+# log = new_log_entries
+
+#display_metrics = ["p@1","p@5","p@10","p@50","r@1","r@5","r@10","r@50"]
+display_metrics = ["p@1","p@5","p@10","p@50"]
+a = pd.DataFrame([[ le['filename'], *le["eval_dataframe"].loc["total (mean)", display_metrics] ] for le in evaluation_log], columns=["short name", *display_metrics])
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+#print(a[-20:len(a)])
+print(a)
+#print(a.iloc[[1,23,36,49]]) # highscores for each method categorie
+print("# TODO change function names to match paper!!! -> revesed order of keys")
